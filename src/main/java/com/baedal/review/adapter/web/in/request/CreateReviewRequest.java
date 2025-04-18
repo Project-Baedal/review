58762610a9cf2c4c810cb5bc6d29b5fc6e9ee0f1
handsone-u <handsoneu@gmail.com>
2025-04-18 21:07:34 +0900

    [#1]  fix: 리뷰 조회 요청 스펙
    - 기본 생성자가 없으면 오류남.

--- src/main/java/com/baedal/review/adapter/web/in/request/CreateReviewRequest.java
+++ src/main/java/com/baedal/review/adapter/web/in/request/CreateReviewRequest.java
@@ -2,23 +2,13 @@ package com.baedal.review.adapter.web.in.request;
 
 import com.baedal.review.adapter.persistence.entity.ReviewScore;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class CreateReviewRequest {
+public record CreateReviewRequest(
+    Long customerId, // TODO: customerId는 Security ContextHolder에서 가져오기로.
+    Long storeId,
+    Long orderId,
+    ReviewScore score,
+    String content,
+    List<String> attchments) {
 
-  // TODO: customerId는 Security ContextHolder에서 가져오기로.
-  private final Long customerId;
-
-  private final Long storeId;
-
-  private final Long orderId;
-
-  private final ReviewScore score;
-
-  private final String content;
-
-  private final List<String> attchments;
 }
