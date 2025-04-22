4536a799d4f1f46710819bf75af0f4e2e8fb0e6e
handsone-u <handsoneu@gmail.com>
2025-04-22 13:44:48 +0900

    [#1] add: openFeign 의존성 추가

--- src/main/java/com/baedal/review/ReviewApplication.java
+++ src/main/java/com/baedal/review/ReviewApplication.java
@@ -2,10 +2,12 @@ package com.baedal.review;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableFeignClients
 public class ReviewApplication {
 
   public static void main(String[] args) {
